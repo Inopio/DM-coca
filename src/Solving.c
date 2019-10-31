8c2b18af75c1c2e50efb6fc13669efce21b537c9
Inopia <vasawin09@gmail.com>
2019-11-01 00:02:52 +0100

    construction de l'affichage du graphe solution à terminer, le reste est ok

--- src/Solving.c
+++ src/Solving.c
@@ -145,37 +145,40 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
         }
     }
 
-    Z3_ast args1[10];
-    Z3_ast save3[10];
-    bool test =false;
     //Phi 5
+    Z3_ast args1[2];
+    Z3_ast savephi5_or[2];
+    Z3_ast savephi5_and[2];
         for(int i=0; i<numGraphs; i++){
             if(pathLength > 1){
                 for(int j=0; j<pathLength; j++){
+                    Z3_ast f5_or;
                     for(int q =0; q<orderG(graphs[i]); q++){
                         for(int r =0; r<orderG(graphs[i]); r++){
-                            //printf("tour %d\n",r);
-                            if(isTarget(graphs[i],r)){
-                                tsave = getNodeVariable(ctx, i, pathLength, pathLength, r);
-                            }
                             if(isEdge(graphs[i],q,r)){
                                 args1[0] = getNodeVariable(ctx, i, j, pathLength, q);
                                 args1[1] = getNodeVariable(ctx, i, j+1, pathLength, r);
                                 tmp = Z3_mk_and(ctx, 2, args1);
-                                if(Z3_is_eq_ast(ctx,tsave,args1[1])){
-                                    test = true;
-                                }
-                                if(save3[0] == NULL){
-                                        f5 = tmp;
-                                        save3[0] = tmp;
-                                        save3[1] = f5;
+                                if(savephi5_or[0] == NULL){
+                                    f5_or = tmp;
+                                    savephi5_or[0] = f5_or;
                                 }else{
-                                        save3[0] = tmp;
-                                        f5=Z3_mk_and(ctx, 2, save3);
-                                        save3[1] = f5;
+                                    savephi5_or[1] = tmp;
+                                    f5_or = Z3_mk_or(ctx, 2, savephi5_or);
+                                    savephi5_or[0] = f5_or;
                                 }
                             }
                         }
+                    }
+                    savephi5_or[0] = NULL;
+                    savephi5_or[1] = NULL;
+                    if(savephi5_and[0] == NULL){
+                        savephi5_and[0] = f5_or;
+                        f5 = f5_or;
+                    }else{
+                        savephi5_and[1] = f5_or;
+                        f5 = Z3_mk_and(ctx, 2, savephi5_and);
+                        savephi5_and[0] = f5;
                     }
                 }
             }else{
@@ -183,30 +186,28 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
                     x1 = getNodeVariable(ctx, i, 0, pathLength, 0);
                     x2 = getNodeVariable(ctx, i, pathLength, pathLength, 1);
                     tmp = Z3_mk_and(ctx, 2, args1);
-                    test = true;
-                    if(save3[0] == NULL){
+                    //test = true;
+                    if(savephi5_and[0] == NULL){
                             f5 = tmp;
-                            save3[0] = tmp;
-                            save3[1] = f5;
+                            savephi5_and[0] = tmp;
                     }else{
-                            save3[0] = tmp;
-                            f5=Z3_mk_and(ctx, 2, save3);
-                            save3[1] = f5;
+                            savephi5_and[0] = tmp;
+                            f5=Z3_mk_and(ctx, 2, savephi5_and);
+                            savephi5_and[1] = f5;
                     }
                 }else{
                     return Z3_mk_false(ctx);
                 }
             }
         }
-    
-    if(!test) return Z3_mk_false(ctx);
+
     fargs[0] = f1;
     fargs[1] = f2;
     fargs[2] = f3;
     fargs[3] = f4;
-    //fargs[3] = f5;
+    fargs[4] = f5;
     
-    f = Z3_mk_and(ctx,4,fargs);
+    f = Z3_mk_and(ctx,5,fargs);
     return  f;
 }
 
@@ -220,6 +221,7 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
  */
 Z3_ast graphsToFullFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs){
     Z3_ast f;
+
     //Pour obtenir la limite de k, majorée par le plus petit des graphes 
     int min_size;
     
@@ -230,7 +232,6 @@ Z3_ast graphsToFullFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
         }
     }
     
-    printf("minsize %d\n",min_size);
     for(int j=0; j<min_size; j++){
         f = graphsToPathFormula(ctx,graphs,numGraphs,j);   //longueur commune de taille j
         if(isFormulaSat(ctx,f)==1) { //si on a une longueur commune de taille j (f satisfiable)
@@ -249,12 +250,28 @@ Z3_ast graphsToFullFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
  * @return int The length of a common simple accepting path in all graphs from @p graphs.
  */ 
 int getSolutionLengthFromModel(Z3_context ctx, Z3_model model, Graph *graphs){
-    /*
-    Z3_ast f = graphsToFullFormula(ctx,Graphs,2);   //fixé pour le moment mais pas bon
-    printf("val : %d\n",Z3_ast_to_string(ctx,y),valueOfVarInModel(ctx,model,f));
-    return valueOfVarInModel(ctx,model,f);
-    */
-   return 42;
+    int max =0;
+    //Pour obtenir la limite de k, majorée par le plus petit des graphes 
+    int min_size;
+    min_size = orderG(graphs[0]);
+    for(int i =1; i < 2; i++){
+        if(min_size < orderG(graphs[i])){
+            min_size = orderG(graphs[i]);
+        }
+    }
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<min_size; j++){
+            for(int k=0; k<min_size; k++){
+                for(int q=0; q<orderG(graphs[i]); q++){
+                    if(valueOfVarInModel(ctx,model,getNodeVariable(ctx,i,j,k,q)) == true){
+                        max = k;
+                    }
+                }
+            }
+        }
+    }
+    return max;
 }
 
 /**
@@ -266,7 +283,21 @@ int getSolutionLengthFromModel(Z3_context ctx, Z3_model model, Graph *graphs){
  * @param numGraph The number of graphs in @p graphs.
  * @param pathLength The length of path.
  */
-void printPathsFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength);
+void printPathsFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength){
+    for(int i=0; i<numGraph; i++){
+        printf("Path from graph %d: ",i);
+        for(int j=0; j<pathLength; j++){
+            for(int k=0; k<=pathLength; k++){
+                for(int q=0; q<orderG(graphs[i]); q++){
+                    if(valueOfVarInModel(ctx,model,getNodeVariable(ctx,i,j,k,q)) == true){
+                        printf("%s ",getNodeName(graphs[i],q));
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
+}
 
 /**
  * @brief Creates the file ("%s-l%d.dot",name,pathLength) representing the solution to the problem described by @p model, or ("result-l%d.dot,pathLength") if name is NULL.
@@ -278,4 +309,30 @@ void printPathsFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numG
  * @param pathLength The length of path.
  * @param name The name of the output file.
  */
-void createDotFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength, char* name);
+void createDotFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength, char* name){
+    char s[1024];
+    sprintf(s,"%s-l%d.dot",name,pathLength);
+    FILE *f = fopen(s,"w+");
+
+    sprintf(s,"digraph %s {\n",name);
+    fputs(s, f);
+    //&s = NULL;
+    fputs("s1 [initial=1,color=green];\n",f);
+	fputs("t1 [final=1,color=red];\n",f);
+    for(int i=0; i<numGraph; i++){
+        for(int j=0; j<pathLength; j++){
+            for(int k=0; k<=pathLength; k++){
+                for(int q=0; q<orderG(graphs[i]); q++){
+                    if(valueOfVarInModel(ctx,model,getNodeVariable(ctx,i,j,k,q)) == true){
+                        sprintf(s,"%s -> ",getNodeName(graphs[i],q));
+                        fputs(s,f);
+                    }
+                }
+            }
+        }
+        fputs("\n",f);
+    }
+    fputs("}",f);
+    fclose(f);
+}
+
--- src/main.c
+++ src/main.c
@@ -10,35 +10,42 @@ int main (){
     Z3_ast a = getNodeVariable(c,1,2,3,4);
     
     Graph graph[2];
-    //graph[0] = getGraphFromFile("graphs/assignment-instance/G1.dot");
-    graph[0] = getGraphFromFile("graphs/assignment-instance/triangle.dot");
+    graph[0] = getGraphFromFile("graphs/assignment-instance/G1.dot");
+    //graph[0] = getGraphFromFile("graphs/assignment-instance/triangle.dot");
+    graph[1] = getGraphFromFile("graphs/assignment-instance/triangle.dot");
     //graph[0] = getGraphFromFile("graphs/generic-instances/positive-instances/Taille10_pos/instance1/G1.dot");
    // graph[0] = getGraphFromFile("graphs/assignment-instance/G1.dot");
    // graph[3] = getGraphFromFile("graphs/assignment-instance/triangle.dot");
    // graph[0] = getGraphFromFile("/net/cremi/sportejoie/espaces/travail/S7/Complexité Calculabilité/assignment/src/G1.dot");
    // graph[0] = getGraphFromFile("graphs/generic-instances/negative-instances/Taille30_neg/instance3/G1.dot");
-    //graph[0] = getGraphFromFile("graphs/generic-instances/positive-instances/Taille20_pos/instance1/G1.dot");
+    //graph[1] = getGraphFromFile("graphs/generic-instances/positive-instances/Taille20_pos/instance1/G1.dot");
    // graph[0] = getGraphFromFile("graphs/generic-instances/negative-instances/Taille10_neg/instance2/G3.dot");   // longeur 0 oui
 
     //printGraph(graph[0]);
 
-    
-    Z3_ast f = graphsToPathFormula(c,graph,1,2);
+    /*  OK
+    Z3_ast f = graphsToPathFormula(c,graph,2,2);
     printf("Graph formula from : %s\n",Z3_ast_to_string(c,f));
+    
 
     Z3_lbool isSat = isFormulaSat(c,f);
     printf("Is sat ? : %d\n",isSat);
 
+    */
     
-    //Z3_ast f2 = graphsToFullFormula(c,graph,2);
-    //int isSat2 = isFormulaSat(c,f2);
-    //printf("Is sat ? : %d\n",isSat2);
+    Z3_ast f2 = graphsToFullFormula(c,graph,2);
+    int isSat2 = isFormulaSat(c,f2);
+    printf("Is sat ? : %d\n",isSat2);
     
-    if(isSat ==1){
-        Z3_model model = getModelFromSatFormula(c,f);
+    if(isSat2 ==1){
+        Z3_model model = getModelFromSatFormula(c,f2);
         int k = getSolutionLengthFromModel(c,model,graph);
         printf("Longueur commune %d\n",k);
+        printPathsFromModel(c,model,graph,2,k);
+        //Z3_del_model(c,model);
+        createDotFromModel(c,model,graph,2,2,"toto");
     }
+    
     
     Z3_del_context(c);
 }
